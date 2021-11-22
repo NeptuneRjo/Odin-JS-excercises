d1e653951500694f52e4a09b2cd7d052a28cc325
NeptuneRjo <rjo9000@outlook.com>
2021-11-21 20:53:13 -0500

    reverseString function build; passed first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
 const reverseString = function(str) {
-
+    let strArray = str.split("");
+    strArray = strArray.reverse();
+    return strArray.join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
