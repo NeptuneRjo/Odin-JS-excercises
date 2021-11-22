b93fec5717e0357dfcccd8f3f2fb2a665dc02c0f
NeptuneRjo <rjo9000@outlook.com>
2021-11-22 18:25:29 -0500

    removeFromArray function written, passed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+// get array 
+// get variables for removal from array
+// iterate through each value in array
+// if value is the same as entered variable, remove
+// after iterating through array, return to new array
+// return new array 
 
+
+const removeFromArray = function(defaultArray, removeVar1, removeVar2) {
+    let newArray = defaultArray;
+
+    for (let i = 0; i <= newArray.length; i++) {
+        if ((newArray[i] == removeVar1) || (newArray[i] == removeVar2)) {
+            newArray.splice(i, i-1);
+        }
+    }
+    
+    return newArray
 };
+
+// console.log(removeFromArray([1,2,3,4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
