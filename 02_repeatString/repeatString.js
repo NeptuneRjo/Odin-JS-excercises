1faaac076fba8efd76f30bf11b9bfe2a77602b90
NeptuneRjo <rjo9000@outlook.com>
2021-11-21 20:12:53 -0500

    completed repeatString function, passed first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeatNum) {
+    let newString = string;
 
+    for (i = 1; i <= repeatNum - 1; i++) {
+        newString += string;
+    }
+
+    return newString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
