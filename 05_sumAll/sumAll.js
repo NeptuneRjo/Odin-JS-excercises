f13a01cceceafad99c885471ffc38c18453ab048
NeptuneRjo <rjo9000@outlook.com>
2021-11-23 23:00:00 -0500

    passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,6 @@
 const sumAll = function(value1, value2) {
     sumOfValues = 0;
 
-    // if (value1 > value2) {
-    //     for (i = value1; i >= value2; i--) {
-    //         sumOfValues += i;
-    //     }
-    // } else if ((typeof(value1, value2) != "number")) {
-    //     return sumOfValues = "ERROR"
-
-    // } else if ((value1 < 0) || (value2 < 0)) {
-    //     return sumOfValue = "ERROR"
-
-    // } else {
-    //     for (i = value1; i <= value2; i++) {
-    //         sumOfValues += i;
-    //     }
-    // }
-
-
     if ((typeof(value1)!== "number" || typeof(value2)!== "number")) {
         return sumOfValues = "ERROR"
 
@@ -31,14 +14,12 @@ const sumAll = function(value1, value2) {
 
     } else {
         for (i = value1; i <= value2; i++) {
-            console.log(value1, value2);
             sumOfValues += i;
         }
     }
 
     return sumOfValues
 };
-
 
 
 // Do not edit below this line
