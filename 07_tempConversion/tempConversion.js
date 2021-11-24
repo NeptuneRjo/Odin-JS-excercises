b4740733be02f048bf96a5a165da852e3f3cecd7
NeptuneRjo <rjo9000@outlook.com>
2021-11-24 01:17:25 -0500

    functions written; passed first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const ftoc = function() {
+const ftoc = function(fahrenheitTemp) {
+  let convertedTemp = ((fahrenheitTemp - 32) * (5/9));
+  return Math.round(convertedTemp * 10) / 10
 
 };
 
-const ctof = function() {
+const ctof = function(celsiusTemp) {
+  let convertedTemp = ((celsiusTemp * (9/5)) + 32);
+  return Math.round(convertedTemp * 10) / 10
 
 };
 
