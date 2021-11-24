cd23bcd6bed3e4efb470a26ffa52c75a6e2b9494
NeptuneRjo <rjo9000@outlook.com>
2021-11-24 00:09:22 -0500

    finished function; passed first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let yearRemainder = year % 4;
+
+    if (year < 1582) {
+        return false
+    } else {
+        if (yearRemainder != 1) {
+            if (yearRemainder == 0) {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
 
 };
 
