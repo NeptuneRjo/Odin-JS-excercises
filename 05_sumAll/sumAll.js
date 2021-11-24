fffa4fdd1964702e39d6e5caa44172a5dec4ff6b
NeptuneRjo <rjo9000@outlook.com>
2021-11-23 21:46:27 -0500

    function written; passes first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    sumOfValues = 0;
 
+    for (i = value1; i <= value2; i++) {
+        sumOfValues += i;
+    }
+
+    return sumOfValues
 };
+
+console.log(sumAll(12, 13))
 
 // Do not edit below this line
 module.exports = sumAll;
