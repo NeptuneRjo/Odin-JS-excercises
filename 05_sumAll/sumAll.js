d07d949147d612a64804226d90b0faf38a2c3199
NeptuneRjo <rjo9000@outlook.com>
2021-11-23 22:24:24 -0500

    re-structured checks in correct order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,33 @@
 const sumAll = function(value1, value2) {
     sumOfValues = 0;
 
-    if (value1 > value2) {
+    // if (value1 > value2) {
+    //     for (i = value1; i >= value2; i--) {
+    //         sumOfValues += i;
+    //     }
+    // } else if ((typeof(value1, value2) != "number")) {
+    //     return sumOfValues = "ERROR"
+
+    // } else if ((value1 < 0) || (value2 < 0)) {
+    //     return sumOfValue = "ERROR"
+
+    // } else {
+    //     for (i = value1; i <= value2; i++) {
+    //         sumOfValues += i;
+    //     }
+    // }
+
+
+    if ((typeof(value1 != "number") || typeof(value2 != "number"))) {
+        return sumOfValues = "ERROR"
+
+    }  else if ((value1 < 0) || (value2 < 0)) {
+        return sumOfValue = "ERROR"
+
+    } else if (value1 > value2) {
         for (i = value1; i >= value2; i--) {
             sumOfValues += i;
         }
-    } else if ((typeof(arguements) != "number") || (arguements < 0)) {
-        return sumOfValues = "ERROR"
-
     } else {
         for (i = value1; i <= value2; i++) {
             sumOfValues += i;
@@ -17,7 +37,7 @@ const sumAll = function(value1, value2) {
     return sumOfValues
 };
 
-console.log(sumAll(123, 1))
+console.log(sumAll(-1, 2))
 
 // Do not edit below this line
 module.exports = sumAll;
