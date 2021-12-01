a51fdf143b3d2524e73179cce054491907db9445
NeptuneRjo <rjo9000@outlook.com>
2021-12-01 18:42:26 -0500

    passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(var1, var2) {
+  return Number(var1 + var2)
 };
 
-const subtract = function() {
-	
+const subtract = function(var1, var2) {
+	return Number(var1 - var2)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0;
+  }
+  return array.reduce((a, b) => a+b)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a*b)
 };
 
-const power = function() {
-	
+const power = function(var1, var2) {
+	return Number(var1 ** var2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+    return -1;
+
+  } else if (num == 0) {
+    return 1;
+
+  } else {
+    return (num * factorial(num-1));
+  }
 };
 
 // Do not edit below this line
